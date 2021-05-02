3b272577f706ee585da2863fcbf11a35955cc3be
Thom Pijnenburg <tspijnenburg@gmail.com>
2021-05-02 18:12:10 +0200

    purify topo feat generation pipeline

--- lynks/data.py
+++ lynks/data.py
@@ -13,95 +13,22 @@ from .logging import get_logger
 logger = get_logger(__name__)
 
 
-class Dataset(object):
-    def __init__(self, lpdata: dict, samples: list):
-        logger.info("I am custom you know")
-
-        self.edges = lpdata["edge"]
-        self.edge_weight_label = "weight"
-        self.edge_year_label = "year"
-        self.edge_attributes = {
-            self.edge_weight_label: lpdata[self.edge_weight_label],
-            self.edge_year_label: lpdata[self.edge_year_label]
-        }
-        self.graph = None
-
-        self.samples = samples
-        self.labels = np.ones(len(samples))
-
-        self.features = []
-        self.feature_names = []
-
-
-def split_list(a, n):
-    k, m = divmod(len(a), n)
-    return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
-
-
-def split_dataset(dataset: Dataset, n_slices: int) -> list:
-    samples = dataset.samples
-    split_samples = split_list(samples, n_slices)
-    result = []
-
-    for split in split_samples:
-        dataset_copy = deepcopy(dataset)
-        dataset_copy.samples = split
-        result.append(dataset_copy)
-
-    return result
-
-
-def merge_datasets(datasets: list) -> Dataset:
-
-    def lambda_merge(d0, d1):
-        d0.samples = d0.samples + d1.samples
-        d0.features = np.concatenate((d0.features, d1.features), axis=0)
-        return d0
-
-    return reduce(lambda_merge, datasets)
-
-
-def _build_graph(edges, weights, years) -> nx.MultiGraph:
+def build_graph(edges, weights, years) -> nx.MultiGraph:
     M = nx.MultiGraph()
 
-    attributes = np.concatenate(([weights],[years]), axis=0)
+    attributes = np.concatenate(([weights], [years]), axis=0)
     edges_w_attributes = np.concatenate((edges, attributes.T), axis=1)
 
     for item in edges_w_attributes:
         M.add_edge(item[0], item[1], weight=item[2], year=item[3])
 
     # turn into simple graph
-
+    # TODO should this be multidigraph?
     G = nx.Graph()
-    for u,v,data in M.edges(data=True):
+    for u, v, data in M.edges(data=True):
         w = data['weight'] if 'weight' in data else 1.0
-        if G.has_edge(u,v):
+        if G.has_edge(u, v):
             G[u][v]['weight'] += w
         else:
             G.add_edge(u, v, weight=w)
     return G
-
-
-def create_graph_builder() -> Callable:
-    def build_graph(dataset: Dataset) -> Dataset:
-        edges = dataset.edges
-        weights = dataset.edge_attributes[dataset.edge_weight_label]
-        years = dataset.edge_attributes[dataset.edge_year_label]
-
-        dataset.graph = _build_graph(edges=edges, weights=weights, years=years)
-        dataset.nodes = list(dataset.graph.nodes)
-        # dataset.samples = list(dataset.graph.edges)
-        return dataset
-    return build_graph
-
-
-def create_formatter() -> Callable:
-    def formatter(data: tuple) -> Dataset:
-        edge_object = data[0]
-        samples = data[1]
-
-        # could do something custom here
-        dataset = Dataset(lpdata=edge_object, samples=samples)
-        return dataset
-
-    return formatter
--- lynks/features.py
+++ lynks/features.py
@@ -2,13 +2,13 @@ import joblib
 import networkx as nx
 import numpy as np
 
-from collections.abc import Callable
+# from collections.abc import Callable
 from collections.abc import Iterable
 from pathlib import Path
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 
-from typing import Union
+from typing import Union, Callable
 
 from .pipeline import create_pipeline
 from .data import Dataset
@@ -19,20 +19,19 @@ logger = get_logger(__name__)
 
 
 def pipeline_cartridge_for_feature(feature_function: Callable) -> Callable:
-    def mutating_feature_function(graph: nx.Graph, edge_tuples: Iterable, features: Iterable, feature_labels: Iterable) -> tuple:
-        feat_values, feat_slug = feature_function(graph, edge_tuples)
+    def mutating_feature_function(edge_tuples: Iterable, features: Iterable, feature_labels: Iterable) -> tuple:
+        feat_values, feat_slug = feature_function(edge_tuples)
         features.append(feat_values)
         feature_labels.append(feat_slug)
-        return graph, edge_tuples, features, feature_labels
+        return edge_tuples, features, feature_labels
 
     return mutating_feature_function
 
 
 def compute_lp_feature(graph: nx.Graph, edge_tuples: Iterable, lp_callback: Callable, verbose: int = 0) -> np.array:
-
     try:
         feats = lp_callback(graph, edge_tuples)
-        feat_values = np.array([p for _, _, p in feats]) # todo maybe return the nodes as well
+        feat_values = np.array([p for _, _, p in feats])  # todo maybe return the nodes as well
     except:
         logger.warn("Could not compute {}".format(str(lp_callback)))
         feat_values = np.full(len(edge_tuples), np.nan)
@@ -58,10 +57,10 @@ def compute_lp_feature(graph: nx.Graph, edge_tuples: Iterable, lp_callback: Call
 #     return feat_common_neighbor_count
 
 
-def create_feature_common_neighbor_centrality(verbose: int = 0):
+def create_feature_common_neighbor_centrality(graph: nx.Graph, verbose: int = 0) -> Callable:
     label = "common_neighbor_centrality"
 
-    def feat_common_neighbor_centrality(graph:nx.Graph, edge_tuples: Iterable) -> tuple:
+    def feat_common_neighbor_centrality(edge_tuples: Iterable) -> tuple:
         if verbose > 0:
             logger.info("Computing {}...".format(label))
         feat_values = compute_lp_feature(graph, edge_tuples, nx.common_neighbor_centrality, verbose=verbose)
@@ -70,10 +69,10 @@ def create_feature_common_neighbor_centrality(verbose: int = 0):
     return feat_common_neighbor_centrality
 
 
-def create_feature_jaccard_coefficient(verbose: int = 0):
+def create_feature_jaccard_coefficient(graph: nx.Graph, verbose: int = 0) -> Callable:
     label = "jaccard_coefficient"
 
-    def feat_jaccard_coefficient(graph:nx.Graph, edge_tuples: Iterable) -> tuple:
+    def feat_jaccard_coefficient(edge_tuples: Iterable) -> tuple:
         if verbose > 0:
             logger.info("Computing {}...".format(label))
         feat_values = compute_lp_feature(graph, edge_tuples, nx.jaccard_coefficient, verbose=verbose)
@@ -82,10 +81,10 @@ def create_feature_jaccard_coefficient(verbose: int = 0):
     return feat_jaccard_coefficient
 
 
-def create_feature_adamic_adar_index(verbose: int = 0):
+def create_feature_adamic_adar_index(graph: nx.Graph, verbose: int = 0):
     label = "adamic_adar_index"
 
-    def feat_adamic_adar_index(graph:nx.Graph, edge_tuples: Iterable) -> tuple:
+    def feat_adamic_adar_index(edge_tuples: Iterable) -> tuple:
         if verbose > 0:
             logger.info("Computing {}...".format(label))
         feat_values = compute_lp_feature(graph, edge_tuples, nx.adamic_adar_index, verbose=verbose)
@@ -94,10 +93,10 @@ def create_feature_adamic_adar_index(verbose: int = 0):
     return feat_adamic_adar_index
 
 
-def create_feature_preferential_attachment(verbose: int = 0):
+def create_feature_preferential_attachment(graph: nx.Graph, verbose: int = 0):
     label = "preferential_attachment"
 
-    def feat_preferential_attachment(graph:nx.Graph, edge_tuples: Iterable) -> tuple:
+    def feat_preferential_attachment(edge_tuples: Iterable) -> tuple:
         if verbose > 0:
             logger.info("Computing {}...".format(label))
         feat_values = compute_lp_feature(graph, edge_tuples, nx.preferential_attachment, verbose=verbose)
@@ -106,10 +105,10 @@ def create_feature_preferential_attachment(verbose: int = 0):
     return feat_preferential_attachment
 
 
-def create_feature_resource_allocation_index(verbose: int = 0):
+def create_feature_resource_allocation_index(graph: nx.Graph, verbose: int = 0):
     label = "resource_allocation_index"
 
-    def feat_resource_allocation_index(graph:nx.Graph, edge_tuples: Iterable) -> tuple:
+    def feat_resource_allocation_index(edge_tuples: Iterable) -> tuple:
         if verbose > 0:
             logger.info("Computing {}...".format(label))
         feat_values = compute_lp_feature(graph, edge_tuples, nx.resource_allocation_index, verbose=verbose)
@@ -119,75 +118,74 @@ def create_feature_resource_allocation_index(verbose: int = 0):
 
 
 def create_feature_formatter(verbose: int = 0):
-    def feature_formatter(dataset: Dataset) -> Dataset:
+    def feature_formatter(feature_list: list, feature_labels: list) -> np.array:
         features = []
-        for index, feat in enumerate(dataset.features):
+        for index, feat in enumerate(feature_list):
             if not np.isnan(feat).any():
                 features.append(np.array([feat]).T)
             else:
-                dataset.feature_names.pop(index)
+                feature_labels.pop(index)
         feats_np = np.concatenate(features, axis=1)
-        dataset.features = feats_np
 
         if verbose > 0:
             logger.info("Length feat list = {} by {}".format(len(features), len(features[0])))
             logger.info("Shape feat array {}".format(feats_np.shape))
 
-        return dataset
+        return feats_np, feature_labels
 
     return feature_formatter
 
 
-
-def create_graph_topology_featuriser(common_neighbors_count: bool, common_neighbor_centrality: bool, jaccard_coefficient: bool,
-                      adamic_adar_index: bool, preferential_attachment: bool, resource_allocation_index: bool,
-                      verbose:int=0) -> Callable:
-    def graph_topology_featuriser(graph_edges: list, edges_to_feat: Union[None, Iterable] = None) -> tuple:
+def create_graph_topology_featuriser(
+        graph_backbone: nx.Graph, common_neighbors_count: bool, common_neighbor_centrality: bool,
+        jaccard_coefficient: bool, adamic_adar_index: bool, preferential_attachment: bool,
+        resource_allocation_index: bool, verbose: int = 0) -> Callable:
+    def graph_topology_featuriser(edges_to_feat: Union[None, Iterable] = None) -> tuple:
         features = []
         feature_labels = []
-        
+
         feature_steps = []
 
         # if common_neighbors_count:
         #     feature_steps.append(create_feature_common_neighbors_count(verbose=verbose))
 
         if common_neighbor_centrality:
-            feature_steps.append(create_feature_common_neighbor_centrality(verbose=verbose))
+            feature_steps.append(create_feature_common_neighbor_centrality(graph_backbone, verbose=verbose))
 
         if jaccard_coefficient:
-            feature_steps.append(create_feature_jaccard_coefficient(verbose=verbose))
+            feature_steps.append(create_feature_jaccard_coefficient(graph_backbone, verbose=verbose))
 
         if adamic_adar_index:
-            feature_steps.append(create_feature_adamic_adar_index(verbose=verbose))
+            feature_steps.append(create_feature_adamic_adar_index(graph_backbone, verbose=verbose))
 
         if preferential_attachment:
-            feature_steps.append(create_feature_preferential_attachment(verbose=verbose))
+            feature_steps.append(create_feature_preferential_attachment(graph_backbone, verbose=verbose))
 
         if resource_allocation_index:
-            feature_steps.append(create_feature_resource_allocation_index(verbose=verbose))
+            feature_steps.append(create_feature_resource_allocation_index(graph_backbone, verbose=verbose))
 
         mutating_feature_steps = map(pipeline_cartridge_for_feature, feature_steps)
 
         feat_pipe = create_pipeline(mutating_feature_steps)
 
-        return feat_pipe(graph_edges, edges_to_feat, features, feature_labels)
+        return feat_pipe(edges_to_feat, features, feature_labels)
 
     return graph_topology_featuriser
 
 
-def create_feature_transform(scaling:str):
-    def transform(dataset: Dataset, pretrained_scaler_path: Path = None) -> Dataset:
-        feats_np = dataset.features
+def create_feature_transform(scaling: str, load_scaler: Path = None):
+    def transform(feats: np.array) -> np.array:
+        # TODO: implement load_scaler
+        feats_transformed = None
 
         if scaling == "normalise":
             scaler = StandardScaler()
-            feats_np = scaler.fit_transform(feats_np)
+            feats_transformed = scaler.fit_transform(feats)
 
         if scaling == "minmax":
             scaler = MinMaxScaler()
-            feats_np = scaler.fit_transform(feats_np)
-        dataset.features = feats_np
+            feats_transformed = scaler.fit_transform(feats)
 
-        return dataset
+        return feats_transformed
 
     return transform
